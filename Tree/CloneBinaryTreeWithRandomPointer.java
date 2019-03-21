5140238fb9df55ce0b8ba676f7e7d0d3528bbcae
Gaurav <48788766+gauravrathee@users.noreply.github.com>
2019-03-21 17:49:14 +0530

    Update CloneBinaryTreeWithRandomPointer.java

--- Tree/CloneBinaryTreeWithRandomPointer.java
+++ Tree/CloneBinaryTreeWithRandomPointer.java
@@ -25,33 +25,33 @@ static TreeNode copyLeftRight(TreeNode root,HashMap<TreeNode,TreeNode> hm)
         if(cloned==null)
         return;
         cloned.random=hm.get(root.random);
-      copyRandom(root.left,cloned.left,hm);
-    copyRandom(root.right,cloned.right,hm);
+        copyRandom(root.left,cloned.left,hm);
+        copyRandom(root.right,cloned.right,hm);
     }
     public static TreeNode cloneTree(TreeNode root){
         if(root==null)
         return null;
-     HashMap<TreeNode,TreeNode> hm=new HashMap<>();
-     TreeNode cloned=copyLeftRight(root,hm);
-     copyRandom(root,cloned,hm);
-     return cloned;
+        HashMap<TreeNode,TreeNode> hm=new HashMap<>();
+        TreeNode cloned=copyLeftRight(root,hm);
+        copyRandom(root,cloned,hm);
+        return cloned;
      }
      
      public static void main(String[] args)
      {
-     TreeNode root=new TreeNode(1);
-     root.left=new TreeNode(2);
-     root.right=new TreeNode(3);
-     root.left.left=new TreeNode(4);
-     root.left.right=new TreeNode(5);
+        TreeNode root=new TreeNode(1);
+        root.left=new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.left=new TreeNode(4);
+        root.left.right=new TreeNode(5);
      
-     root.random=root.left.right;
-     root.left.random=root;
-     root.right.random=root.left.left;
-     root.left.left.random=root.left.right;
-     root.left.right.random=root;
+        root.random=root.left.right;
+        root.left.random=root;
+        root.right.random=root.left.left;
+        root.left.left.random=root.left.right;
+        root.left.right.random=root;
      
-     TreeNode cloned=cloneTree(root);
+        TreeNode cloned=cloneTree(root);
      
      
      }
